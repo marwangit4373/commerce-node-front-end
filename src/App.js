76d5302e03f6035b66208c425885fb24eb772e1f
marwangit4373 <gitmarwan36@gmail.com>
2024-11-02 13:00:07 +0200

    add front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+        <Route path="/" element={<h1>Product Component</h1>} />
+                  {/* this for add in link */}
+          <Route path="/add" element={<h1> AddProduct Component</h1>} /> 
+          <Route path="/update" element={<h1>Update Product Component</h1>} />
+          <Route path="/logout" element={<h1> Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Product Component</h1>} />
+          <Route path="/signup" element={<SignUp></SignUp>} />
+
+        </Routes>
+
+      </BrowserRouter>
+      <Footer />
+
+
     </div>
   );
 }
